a2f116773021d55827e2d7f6234de3754b546db6
DK <dasunkanishka92@gmail.com>
2023-03-07 23:16:41 +0530

    Add static header and footer

--- next-frontend/components/Layout.jsx
+++ next-frontend/components/Layout.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import Head from 'next/head';
 
-const Layout = () => {
-    return <div>Layout</div>;
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+const Layout = ({ children }) => {
+    return (
+        <div className="layout">
+            <Head>
+                <title>Next/Sanity eCommerce</title>
+            </Head>
+
+            <header>
+                <Navbar />
+            </header>
+
+            <div className="main-container">{children}</div>
+
+            <footer>
+                <Footer />
+            </footer>
+        </div>
+    );
 };
 
 export default Layout;
--- next-frontend/components/Navbar.jsx
+++ next-frontend/components/Navbar.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import Link from 'next/link';
+import { AiOutlineShopping } from 'react-icons/ai';
 
 const Navbar = () => {
-    return <div>Navbar</div>;
+    return (
+        <div className="navbar-container">
+            <p className="logo">
+                <Link href="/">Store Name</Link>
+            </p>
+
+            <button className="cart-icon">
+                <AiOutlineShopping /> <span className="cart-item-qty">01</span>
+            </button>
+        </div>
+    );
 };
 
 export default Navbar;
